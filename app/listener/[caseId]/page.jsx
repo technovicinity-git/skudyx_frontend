b98722046dec7001c63af6073d5c75e99feb5260
Selim Reja <sreja.bd@gmail.com>
2026-04-16 14:57:28 +0600

    dev

--- app/listener/[caseId]/page.jsx
+++ app/listener/[caseId]/page.jsx
@@ -9,8 +9,6 @@ const CaseViewer = () => {
   return (
     <div className="min-h-screen bg-gray-900 text-white p-8">
       <TestAudioPlayer caseId={caseId} />
-
-      <div className="mt-8"></div>
     </div>
   );
 };
